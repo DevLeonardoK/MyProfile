a35782d4d9a84fad4f118b6561394c6bca2a92fb
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2024-05-24 21:49:40 -0300

    togglemode() js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function togglemode() {
+  const html = document.documentElement;
+
+  //Metodo 1
+
+  //   if (html.classList.contains("light")) {
+  //         //Verificando se no HTML, na lista de classes, possui alguma chamada como "light"
+  //     html.classList.remove("light");
+
+  //   }else{
+  //     html.classList.add("light")
+  //   }
+
+  //Metodo 2
+  html.classList.toggle("light");
+  //Remover e adicionar a class "light"
+
+  //img
+  const img = document.querySelector(".profile img");
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "../img/avatar.png");
+  } else {
+    img.setAttribute("src", "../img/avatar-dark.png");
+  }
+}
