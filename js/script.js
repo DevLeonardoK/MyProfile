d97e660649c1f96313bdd209e6c1e66fdfdb1d40
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2024-05-30 16:28:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function togglemode() {
   //img
   const img = document.querySelector(".profile img");
   if (html.classList.contains("light")) {
-    img.setAttribute("src", ".img/avatar.png");
+    img.setAttribute("src", "img/avatar.png");
   } else {
     img.setAttribute("src", "img/avatar-dark.png");
   }
